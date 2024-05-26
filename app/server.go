4d72c0f9ca66d4357425ac1c88d4bf0b8d9d1df1
Pratik Kulkarni <praaatik.kulkarni@proton.me>
2024-05-26 10:27:40 +0530

    stage-5--commit-1

--- app/server.go
+++ app/server.go
@@ -6,10 +6,26 @@ import (
 	"net"
 	"os"
 	"strings"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
 )
+
+func generateResponse(query string) []byte {
+	response := make([]byte, 1024)
+	queryParts := strings.Split(query, " ")[1]
+
+	if len(queryParts) > 2 {
+		arr := strings.Split(queryParts, "/")
+		if len(arr) > 2 {
+			responseBody := arr[len(arr)-1]
+			responseValue := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(responseBody), responseBody)
+			response = []byte(responseValue)
+		} else {
+			response = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+		}
+	} else {
+		response = []byte("HTTP/1.1 200 OK\r\n\r\n")
+	}
+	return response
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -33,12 +49,13 @@ func main() {
 			os.Exit(1)
 		}
 
-		response := make([]byte, 1024)
-		if len(strings.Split(requestLine, " ")[1]) > 1 {
-			response = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
-		} else {
-			response = []byte("HTTP/1.1 200 OK\r\n\r\n")
-		}
+		response := generateResponse(requestLine)
+		// fmt.Println(response)
+		// if len(strings.Split(requestLine, " ")[1]) > 1 {
+		// 	response = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+		// } else {
+		// 	response = []byte("HTTP/1.1 200 OK\r\n\r\n")
+		// }
 		_, err = localConnection.Write(response)
 		localConnection.Close()
 	}
